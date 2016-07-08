e66155e0525269820b9fbe120416fe0474c12a4f
Davide Nunes <davidelnunes@gmail.com>
2016-07-08 14:07:22 +0100

    simplified text transformations module

--- deepsign/test/text/test_transform.py
+++ deepsign/test/text/test_transform.py
@@ -5,35 +5,22 @@ import deepsign.text.transform as transf
 class TestTransform(unittest.TestCase):
 
     def testCaseTransform(self):
-        case = transf.CaseTransform()
 
         s1 = "Hello there Mr Smith welcome back to the World"
 
-        s2 = case.apply(s1.split(" "))
+        s2 = transf.to_lower(s1.split(" "))
+
+        print(s2)
 
     def testCleanPunctuation(self):
-        punct = transf.CleanPunctuation()
 
         s1 = "Hello there Mr Smith , welcome back to the World !"
         s1a = s1.split(" ")
 
-        s2 = punct.apply(s1a)
+        s2 = transf.rm_punctuation(s1a)
         self.assertEqual(len(s1a)-2,len(s2))
 
-    def testTransformPipeline(self):
-        pipe = transf.TransformPipeline()
-        pipe.append(transf.CleanPunctuation())
-        pipe.append(transf.CaseTransform())
-
-        case = transf.CaseTransform()
-
-        s1 = "Hello there Mr Smith welcome back to the World"
-        s2 = "Hello there Mr Smith , welcome back to the World !"
-
-        res1 = case.apply(s1.split(" "))
-        res2 = pipe.apply(s2.split(" "))
-
-        self.assertListEqual(res1,res2)
+        print(s2)
 
 
 if __name__ == '__main__':
