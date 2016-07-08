a9458dd177826b4ce4c5a9654650193dadb7a98f
Davide Nunes <davidelnunes@gmail.com>
2016-07-08 14:05:45 +0100

    simplified text transformations module

--- deepsign/text/transform.py
+++ deepsign/text/transform.py
@@ -2,35 +2,11 @@ import deepsign.text.tokenizer as tk
 import re
 
 
-class Transform:
-    def apply(self,seq=[]):
-        return seq
-
-
-class TransformPipeline:
-    def __init__(self):
-        self.pipeline = []
-
-    def append(self, transform):
-        if not isinstance(transform,Transform):
-            raise Exception("cannot only append transform instances")
-        self.pipeline.append(transform)
-
-    def apply(self,seq):
-        res = seq
-        for tf in self.pipeline:
-            res = tf.apply(res)
-        return res
-
-
-class CaseTransform(Transform):
-    def apply(self,seq=[]):
+def to_lower(seq=[]):
         return [s.lower() for s in seq]
 
 
-class CleanPunctuation(Transform):
-
-    def apply(self,seq=[]):
+def rm_punctuation(seq=[]):
         CLITIC_REGEX = tk.clitic
         WORD_REGEX = "\w+"
         VALID_REGEX = re.compile(tk.regex_or(CLITIC_REGEX,WORD_REGEX))
