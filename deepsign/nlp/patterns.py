197439a07f36b43cca1333e844370e0fd5a14539
Davide Nunes <davidelnunes@gmail.com>
2016-09-15 21:29:43 +0100

    corrected url regex patterns

--- deepsign/nlp/patterns.py
+++ deepsign/nlp/patterns.py
@@ -1,10 +1,9 @@
 """
-DeepSign: Tokenizers
+DeepSign regular expression patterns
 
-RegEx for
+includes patterns for
  * URL
  * E-MAIL
-
 """
 
 # Patterns:
@@ -16,35 +15,44 @@ RegEx for
 import re
 
 
-def _regex_or(*items):
-    return '(?:' + '|'.join(items) + ')'
-
-
 # RFC3986-like URIs
 # also matches e-mail addresses since you can have a user before the host
-URL = re.compile(
-    r"""
-    (?<=[\s(\[\{<"'«‘‛“„‟‹❮`])?                           # url boundary
+# not perfect but accepts and splits most cases
+URL = re.compile(r"""
+    (?:(?<=(?:[^\w\/]))|(?<=(?:^)))                     # boundary !word or part of a path (avoid capturing /f.html)
     (
-          (?:https?://|\bwww\.|[A-z]{2,}:(?://)?)?        # other protocol
-          (?:[A-Za-z\d-]+@)?                              # optional user
-          \b(?:[A-Za-z\d-])+(?:\.[A-Za-z0-9]+){0,3}\.     #required host
-          (?:\w{2,})(?=\W|$)                              # required TLD
-          (?::\d+)?                                       # optional port
-          (?:\/[^?\#\s'">)\]}]+)?                         # optional path
-          (?:\?[^\#\s'">)\]}]+)?                          # optional query
-          (?:\#[^\s'">)\]}]+)?                           # optional fragment
+          (?:https?://|[A-z]{2,}:(?://)?)?              # scheme
+          (?:[A-Za-z0-9_-]+@)?                          # optional user
+          \b(?:[A-Za-z0-9-])+(?:\.[A-Za-z0-9-]+)*       # required host
+          \.
+          (?:\w{2,})(?=\W|$)                            # required TLD ---followed by non-word char (?=\W|$)
+          (?::\d{2,5})?                                 # optional port
+          (?:\/@[A-Za-z0-9_-]+)?                        # optional path with handle (e.g. medium.com/@user)
+          (?:                                           # begin optional path
+            (?:
+                \/[^?\#\s'">\]}.]+
+                (?:\.[\w]+)*                            # an end in ext or contain dots (e.g. file.html or /v0.2.3/)
+            )+                                          # it might occur more than once (/v0.2.1/file.html)
+          )?                                            # end optional path
+          (?:\?[^\#\s'">)\]}]+)?                        # optional query
+          (?:\#[^\s'">)\]}]+)?                          # optional fragment (I don't allow closing parenthesis in fragments
     )
-    (?=[\s)\]\}>"'»’”›❯',]|$)?                            # url boundary
-    """,
-    re.VERBOSE | re.UNICODE)
+    (?=\W|$)                                            # url boundary
+    """, re.VERBOSE | re.UNICODE)
 
-# E-MAIL ***************************************************************************************************************
-_email_bound = r"(?:\W|^|$)"
 
-EMAIL = re.compile(_regex_or("(?<=(?:\W))", "(?<=(?:^))") + r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}(?=" +
-                   _email_bound + ")", re.UNICODE)
+EMAIL = re.compile(r"""
+    (?:(?<=(?:\W))|(?<=(?:^)))                          # e-mail boundary
+    (
+        [a-zA-Z0-9.-_']+                                # user
+        @
+        \b(?:[A-Za-z0-9-])+(?:\.[A-Za-z0-9-]+)*         # host
+        \.
+        (?:\w{2,})                                      # TLD
+    )
+    (?=\W|$)                                            # e-mail boundary
+    """, re.VERBOSE | re.UNICODE)
 
-# Contractions *********************************************************************************************************
+
 
 CONTRACTIONS = r"(?i)(\w+)(n['’′]t|['’′]ve|['’′]ll|['’′]d|['’′]re|['’′]s|['’′]m)$"
--- deepsign/test/nlp/test_patterns.py
+++ deepsign/test/nlp/test_patterns.py
@@ -4,29 +4,90 @@ from deepsign.nlp import token
 from segtok.tokenizer import web_tokenizer
 import re
 
+
+def _matches(pattern, target_str):
+    found = pattern.findall(target_str)
+    return len(found) == 1 and found[0] == target_str
+
 class TestPatterns(unittest.TestCase):
-    def test_url(self):
 
-        url1 = "http://stuff.com?stuff=cenas"
-        url2 = "http://davikjhasds.xyz"
-        url3 = "mailto:theman@theboss.com"
-        email = "esbanhsdoias@mail.com"
 
-        assert(token.is_url(url1))
-        assert(token.is_url(url2))
-        assert(token.is_url(url3))
-        assert(token.is_url(email))
 
-        assert(not token.is_email(url1))
-        assert(not token.is_email(url2))
-        assert(not token.is_email(url3))
-        assert(token.is_email(email))
+    def test_url_uri_email(self):
+        # Examples to be Accepted
 
-        sentence = "stuff and other stuff ahaha (" + url1 + ") final stuff"
+        # URL
+        url_simple = "domain.tld"
+        url_simple_sub = "sub.domain.tld"
+        reject_url_simple_sub = "sub..domain.tld"
+        url_simple_port = "domain.com:8080"
+        url_simple_path1 = "domain.com/file.html"
+        url_simple_path2 = "domain.com/path-path"
+        url_simple_path3 = "domain.com/path+path2+path3"
 
-        url1_re = patterns.URL
+        assert (patterns.URL.match(url_simple) )
+        assert (patterns.URL.match(url_simple_sub))
+        assert (not patterns.URL.match(reject_url_simple_sub))
+        assert (patterns.URL.match(url_simple_port))
+        assert (patterns.URL.match(url_simple_path1))
+        assert (patterns.URL.match(url_simple_path2))
+        assert (patterns.URL.match(url_simple_path3))
+
+
+
+        url_scheme_simple = "https://domain.tld"
+        url_scheme_sub = "https://sub.domain.tld"
+        url_path_asterisk = "http://domain.tld/path/*/http://google.com"
+        url_path_bang = "http://domain.tld/path/!/http://google.com"
+        url_parenthesis_1 = "http://example.com/file%2811%281.html)"
+        url_parenthesis_2 = "http://example.com/@user/file(1).html"
+        url_parenthesis_3 = "http://sub.domain.domain/p1/r2.4.1/file.html#segment(string)"
+
+        assert (patterns.URL.match(url_scheme_simple))
+        assert (patterns.URL.match(url_scheme_sub))
+        assert (patterns.URL.match(url_path_asterisk))
+        assert (patterns.URL.match(url_parenthesis_1))
+        #print(patterns.URL.findall(url_parenthesis_1))
+        #print(patterns.URL.findall(url_parenthesis_2))
+
+        print(patterns.URL.findall(url_path_asterisk))
+        print(patterns.URL.findall(url_path_bang))
+        print(patterns.URL.findall(url_parenthesis_1))
+        print(patterns.URL.findall(url_parenthesis_2))
+        print(patterns.URL.findall(url_parenthesis_3))
+
+        url_fragment = "http://domain.tld/path/#hello"
+        url_handle = "https://domain.tld/@username"
+        url_tilde = "http://domain.tld/~username"
+
+        url_path_query_1 = "domain.com/path?key=value&key2=value2"
+        url_path_query_2 = "domain.com/path?key=value&key2=value_2"
+        url_path_query_3 = "domain.com/path?key=value1+value_2"
+        url_path_query_4 = "domain.com/path/!/value1&value2"
+        url_path_query_5 = "domain.com/path$?key=value1"
+        url_path_query_6 = "domain.com/path?key=value1;value_2"
+        url_path_query_7 = "domain.com/path?key=value1%20value_2"
+
+        uri_user = "user:info@host.name.tld"
+        uri_mail = "mailto:user@mail.tld"
+        uri_full = "(abc://username@example.com:123/path/data?key=value&key2=value2#fragid1)"
+
+        print(patterns.URL.findall(uri_full))
+
+
+        url_ssh_1 = "user1@domain.com:user2/file.ext"
+        url_ssh_2 = "ssh://username@hostname:/path"
+        url_ssh_3 = "ssh://username@hostname:1010/path"
+
+        # E-MAIL
+        email_simple = "user101-a_b@mail.com"
+        email_sub = "yser@one.two.tld"
+        email_dotuser = "user.101@mail.tld"
+
+        # sentence = "stuff and other stuff ahaha (" + url1 + ") final stuff"
+
+        # url1_re = patterns.URL
         email_re = patterns.EMAIL
 
-        print(url1_re.split(sentence))
-        print(web_tokenizer(sentence))
-
+        # print(url1_re.split(sentence))
+        # print(web_tokenizer(sentence))
