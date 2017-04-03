1eaa594770e6fc118b2f2b3ec12e51004abe6255
Davide Nunes <davidelnunes@gmail.com>
2017-04-03 18:48:15 +0100

    scatter update of matrix rows

---
+++ examples/tensorflow_update_rows.py
@@ -0,0 +1,18 @@
+# example for updating the rows of a variable in tensorflow
+
+import tensorflow as tf
+update_tensor = tf.constant([0,0,0,0])
+
+
+a = tf.Variable(initial_value=[[1, 1, 1, 1],[2, 2, 2, 2],[3, 3, 3, 3]])
+b = tf.scatter_update(ref=a, indices=[0], updates=[update_tensor])
+init = tf.global_variables_initializer()
+
+
+
+with tf.Session() as s:
+  s.run(init)
+
+  print("a = ", s.run(a))
+  print("updated = ", s.run(b))
+  print("a = ", s.run(a))
