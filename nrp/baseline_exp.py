e15f24ef06431d52add2eb192b831a1cf533d669
Davide Nunes <davidelnunes@gmail.com>
2018-02-14 19:35:48 +0000

    added option to grou jobs to experiment runner

--- nrp/baseline_exp.py
+++ nrp/baseline_exp.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser(description="NNLM Baseline Parameters")
 parser.add_argument('-grid', choices=['mas', 'ingrid'], default="mas")
 parser.add_argument('-run_script', dest="run_script", type=str, default="run/nnlm.py")
 parser.add_argument('-params', dest="params", type=str, default="baseline")
+parser.add_argument('-group', dest="group", type=int, default=1)
 args = parser.parse_args()
 
 grid_cfg = getattr(grid_confs, args.grid)
@@ -19,4 +20,4 @@ param_space = getattr(baseline_params, args.params)
 print(param_space)
 
 runner = GridRunner(grid_cfg)
-runner.submit_all(args.run_script, param_space, "baseline", None, call_qsub=False)
+runner.submit_all(args.run_script, param_space, "baseline", None, call_qsub=False,group=args.group)
