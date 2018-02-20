6519111a2fd39bd5a1ccc8396b66989d8989035d
= <=>
2018-02-20 00:21:17 +0000

    corrected a bug where lookup was being initted in a hardcoded manner instead of using embed init

--- deepsign/models/models.py
+++ deepsign/models/models.py
@@ -65,7 +65,7 @@ class NNLM(tx.Model):
                                  ctx_size,
                                  embeddings_shape,
                                  batch_size,
-                                 weight_init=tx.random_normal(0, 0.05))
+                                 weight_init=embed_init)
 
         out_layer = lookup_layer
         h_layers = []
@@ -98,9 +98,127 @@ class NNLM(tx.Model):
         train_loss = tx.categorical_cross_entropy(self.loss_inputs.tensor, train_logits.tensor)
 
         # ===============================================
-        # TRAIN GRAPH
+        # EVAL GRAPH
         # ===============================================
         eval_loss = tx.categorical_cross_entropy(self.eval_inputs.tensor, run_logits.tensor)
+        eval_tensors = tf.reduce_mean(eval_loss)
+
+        # BUILD MODEL
+        super().__init__(run_in_layers=self.run_inputs, run_out_layers=run_output,
+                         train_in_layers=self.run_inputs, train_out_layers=train_output,
+                         eval_in_layers=self.run_inputs, eval_out_layers=run_output,
+                         train_loss_tensors=train_loss, train_loss_in=self.loss_inputs,
+                         eval_tensors=eval_tensors, eval_tensors_in=self.eval_inputs)
+
+
+class NRP(tx.Model):
+    def __init__(self,
+                 ctx_size,
+                 ri_size,
+                 embed_dim,
+                 batch_size,
+                 h_dim,
+                 embed_init=tx.random_normal(0, 0.01),
+                 logit_init=tx.random_normal(0, 0.01),
+                 num_h=1,
+                 h_activation=tx.relu,
+                 h_init=tx.relu_init,
+                 use_dropout=False,
+                 keep_prob=0.1,
+                 run_inputs=None,
+                 eval_inputs=None,
+                 loss_inputs=None):
+        # NNLM PARAMS
+        self.embed_init = embed_init
+        self.loss_inputs = loss_inputs
+        self.eval_inputs = eval_inputs
+        self.run_inputs = run_inputs
+        self.h_init = h_init
+        self.h_activation = h_activation
+        self.h_dim = h_dim
+        self.num_h = num_h
+        self.batch_size = batch_size
+        self.embed_dim = embed_dim
+        self.ri_size = ri_size
+        self.n_gram_size = ctx_size
+        self.use_dropout = use_dropout
+        self.keep_prob = keep_prob
+        self.logit_init = logit_init
+
+        if self.run_inputs is None:
+            self.run_inputs = tx.SparseInput(self.ri_size, dtype=tf.float32)
+        elif not isinstance(self.run_inputs, tx.SparseInput):
+            raise TypeError(
+                "expected run_inputs to be a SparseInput layer got {} instead".format(type(self.run_inputs)))
+
+        if self.loss_inputs is None:
+            loss_inputs = tx.SparseInput(self.ri_size * 2, dtype=tf.float32, name="ri_labels_loss")
+            self.loss_inputs = loss_inputs
+        elif not isinstance(self.loss_inputs, tx.SparseInput):
+            raise TypeError(
+                "expected loss_inputs to be a SparseInput layer got {} instead".format(type(self.loss_inputs)))
+
+        if self.eval_inputs is None:
+            eval_inputs = tx.SparseInput(n_units=self.ri_size * 2, name="ri_labels_eval")
+            self.eval_inputs = eval_inputs
+        elif not isinstance(self.eval_inputs, tx.SparseInput):
+            raise TypeError(
+                "expected eval_inputs to be a SparseInput layer got {} instead".format(type(self.eval_inputs)))
+
+        # hidden layer
+        if num_h < 1:
+            raise ValueError("num hidden should be >= 1")
+
+        # ===============================================
+        # RUN GRAPH
+        # ===============================================
+
+        # lookup layer
+        embeddings_shape = [ri_size, embed_dim]
+        lookup_layer = tx.Lookup(self.run_inputs,
+                                 ctx_size,
+                                 embeddings_shape,
+                                 batch_size,
+                                 weight_init=embed_init)
+
+        out_layer = lookup_layer
+        h_layers = []
+        for i in range(num_h):
+            h_i = tx.Linear(out_layer, h_dim, h_init, bias=True, name="h_{i}_linear".format(i=i))
+            h_a = tx.Activation(h_i, h_activation)
+            h = tx.Compose([h_i, h_a], name="h_{i}".format(i=i))
+            h_layers.append(h_i)
+            out_layer = h
+
+        # NRP uses random projections so we compare them with the actual random projections and use sigmoids
+        # to model each feature probability independently
+        # 2 classes of probabilities for positive entries and negative entries (ri_size*2)
+        run_logits = tx.Linear(out_layer, ri_size * 2, init=logit_init, bias=True, name="run_logits")
+        run_output = tx.Activation(run_logits, tx.sigmoid, name="run_output")
+
+        # ===============================================
+        # TRAIN GRAPH
+        # ===============================================
+
+        out_layer = lookup_layer
+
+        # add dropout between each layer
+        for layer in h_layers:
+            h = layer.reuse_with(out_layer)
+            if use_dropout:
+                h = tx.Dropout(h, keep_prob=keep_prob)
+            out_layer = h
+
+        # share logits but connected to train graph (which contains dropout)
+        train_logits = run_logits.reuse_with(out_layer, name="train_logits")
+        train_output = tx.Activation(train_logits, tx.sigmoid, name="train_output")
+        # we need to use binary cross entropy instead of categorical because we consider probabilities independent
+        train_loss = tx.binary_cross_entropy(self.loss_inputs.tensor, train_logits.tensor)
+
+        # ===============================================
+        # EVAL GRAPH
+        # ===============================================
+        eval_loss = tx.binary_cross_entropy(self.eval_inputs.tensor, run_logits.tensor)
         eval_tensors = tf.reduce_mean(eval_loss)
 
         # BUILD MODEL
