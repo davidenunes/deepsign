cde4e95b1999af8414b18524c02d5c6e81aa6f01
= <=>
2018-04-03 23:11:34 +0100

    started nrp model

---
+++ deepsign/models/nrp.py
@@ -0,0 +1,174 @@
+""" Log Bi-Linear Language Model
+
+"""
+import tensorx as tx
+import tensorflow as tf
+from tensorflow.python.ops.candidate_sampling_ops import uniform_candidate_sampler as uniform_sampler
+
+
+class LBLNRP(tx.Model):
+    """ LBL with NRP
+
+    Log BiLinear Model with gating and hidden non-linear layer extensions using the random
+    projection encoding
+
+    Notes:
+        - If embed_share is False, the model uses two embedding layers, one for the context words
+        and one for the target words. The context word embeddings are initialized with the embed_init
+        function. The target word embeddings are initialized with the logit_init function
+
+    Args:
+            ctx_size: number of words in the input context / sequence
+            k_dim dimension for random projections, shoudl match dimension in ri_tensor
+            ri_tensor: a sparse tensor with the sparse random indexes for each word, ordered by word index
+            embed_dim: feature / embedding dimension
+            embed_init: initialisation function for embedding weights
+            embed_share: share the features with target words
+            nce: bool if true uses NCE approximation to softmax with random uniform samples
+            nce_samples: number of samples for nce
+            logit_init: initialisation function for logits only applicable if embed_share is set to False.
+            h_to_f_init: if use hidden we have an additional set of parameters from the hidden layer to f_predict
+
+    """
+
+    def __init__(self,
+                 ctx_size,
+                 vocab_size,
+                 k_dim,
+                 ri_tensor,
+                 embed_dim,
+                 embed_init=tx.random_uniform(minval=-0.01, maxval=0.01),
+                 x_to_f_init=tx.random_uniform(minval=-0.01, maxval=0.01),
+                 logit_init=tx.random_uniform(minval=-0.01, maxval=0.01),
+                 embed_share=True,
+                 use_gate=True,
+                 use_hidden=False,
+                 h_dim=100,
+                 h_activation=tx.elu,
+                 h_init=tx.he_normal_init(),
+                 h_to_f_init=tx.random_uniform(minval=-0.01, maxval=0.01),
+                 use_dropout=True,
+                 embed_dropout=False,
+                 keep_prob=0.95,
+                 l2_loss=False,
+                 l2_loss_coef=1e-5,
+                 use_nce=False,
+                 nce_samples=100):
+
+        # GRAPH INPUTS
+        run_inputs = tx.Input(ctx_size, dtype=tf.int32, name="input")
+        loss_inputs = tx.Input(n_units=1, dtype=tf.int32, name="target")
+        eval_inputs = loss_inputs
+
+        # RUN GRAPH =====================================================
+        var_reg = []
+        with tf.name_scope("run"):
+            # RI ENCODING ===============================================
+            # convert ids to ris gather a set of random indexes based on the ids in a sequence
+            ri_layer = tx.TensorLayer(ri_tensor, n_units=k_dim)
+            ri_inputs = tx.gather_sparse(ri_layer.tensor, run_inputs.tensor)
+            ri_inputs = tx.TensorLayer(ri_inputs, k_dim)
+            # ===========================================================
+
+            # use those sparse indexes to lookup a set of features based on the ri values
+            feature_lookup = tx.Lookup(ri_inputs, ctx_size, [k_dim, embed_dim], embed_init, name="lookup")
+            var_reg.append(feature_lookup.weights)
+
+            if use_gate or use_hidden:
+                hl = tx.Linear(feature_lookup, h_dim, h_init, name="h_linear")
+                ha = tx.Activation(hl, h_activation, name="h_activation")
+                h = tx.Compose([hl, ha], name="hidden")
+                var_reg.append(hl.weights)
+
+            features = feature_lookup
+            if use_gate:
+                features = tx.Gate(features, ctx_size, gate_input=h)
+                gate = features
+                var_reg.append(features.gate_weights)
+
+            x_to_f = tx.Linear(features, embed_dim, x_to_f_init, name="x_to_f")
+            var_reg.append(x_to_f.weights)
+            f_prediction = x_to_f
+
+            if use_hidden:
+                h_to_f = tx.Linear(h, embed_dim, h_to_f_init, name="h_to_f")
+                var_reg.append(h_to_f.weights)
+                f_prediction = tx.Add([x_to_f, h_to_f], name="f_predicted")
+
+            shared_weights = tf.transpose(feature_lookup.weights) if embed_share else None
+            logit_init = logit_init if not embed_share else None
+            # features for all words with shape [ri_layer.shape[0], embed_dim]
+            run_features = tx.Linear(ri_layer, embed_dim, logit_init, shared_weights, name="logits", bias=False)
+
+            # dot product of f_predicted and all features
+            run_logits = tx.Linear(f_prediction,
+                                   n_units=vocab_size,
+                                   shared_weights=tf.transpose(run_features.tensor))
+
+            if not embed_share:
+                var_reg.append(run_logits.weights)
+            y_prob = tx.Activation(run_logits, tx.softmax)
+
+        # TRAIN GRAPH ===================================================
+        with tf.name_scope("train"):
+            if use_dropout and embed_dropout:
+                feature_lookup = feature_lookup.reuse_with(run_inputs)
+                features = tx.Dropout(feature_lookup, keep_prob=keep_prob)
+            else:
+                features = feature_lookup
+
+            if use_gate or use_hidden:
+                if use_dropout:
+                    h = h.reuse_with(features)
+                    h = tx.Dropout(h, keep_prob=keep_prob)
+
+                if use_gate:
+                    features = gate.reuse_with(features, gate_input=h)
+
+                f_prediction = x_to_f.reuse_with(features)
+
+                if use_hidden:
+                    h_to_f = h_to_f.reuse_with(h)
+                    if use_dropout:
+                        h_to_f = tx.Dropout(h_to_f, keep_prob=keep_prob)
+                    f_prediction = tx.Add([f_prediction, h_to_f])
+            else:
+                f_prediction = f_prediction.reuse_with(features)
+
+            train_logits = run_logits.reuse_with(f_prediction)
+
+            if use_nce:
+                # uniform gets good enough results if enough samples are used
+                # but we can load the empirical unigram distribution
+                # or learn the unigram distribution during training
+                sampled_values = uniform_sampler(loss_inputs.tensor, 1, nce_samples, True, vocab_size)
+                train_loss = tf.nn.nce_loss(weights=tf.transpose(train_logits.weights),
+                                            biases=train_logits.bias,
+                                            inputs=f_prediction.tensor,
+                                            labels=loss_inputs.tensor,
+                                            num_sampled=nce_samples,
+                                            num_classes=vocab_size,
+                                            num_true=1,
+                                            sampled_values=sampled_values)
+            else:
+                one_hot = tx.dense_one_hot(column_indices=loss_inputs.tensor, num_cols=vocab_size)
+                train_loss = tx.categorical_cross_entropy(one_hot, train_logits.tensor)
+
+            train_loss = tf.reduce_mean(train_loss)
+
+            if l2_loss:
+                losses = [tf.nn.l2_loss(var) for var in var_reg]
+                train_loss = train_loss + l2_loss_coef * tf.add_n(losses)
+
+        # EVAL GRAPH ===============================================
+        with tf.name_scope("eval"):
+            one_hot = tx.dense_one_hot(column_indices=eval_inputs.tensor, num_cols=vocab_size)
+            eval_loss = tx.categorical_cross_entropy(one_hot, run_logits.tensor)
+            eval_loss = tf.reduce_mean(eval_loss)
+
+        # SETUP MODEL CONTAINER ====================================
+        super().__init__(run_in_layers=run_inputs, run_out_layers=y_prob,
+                         train_in_layers=run_inputs, train_out_layers=y_prob,
+                         eval_in_layers=run_inputs, eval_out_layers=y_prob,
+                         train_loss_tensors=train_loss, train_loss_in=loss_inputs,
+                         eval_tensors=eval_loss, eval_tensors_in=eval_inputs)
--- examples/random_projection_embeddings.py
+++ examples/random_projection_embeddings.py
@@ -38,7 +38,22 @@ inputs = tx.Input(n_units=2)
 ri_inputs = tx.gather_sparse(sp_ri, inputs.tensor)
 ri_inputs = tx.TensorLayer(ri_inputs, k)
 
+
 embed = tx.Lookup(ri_inputs, seq_size, [k, embed_dim])
+
+# logits: take the embeddings and get the features for all random indexes
+
+ri_layer = tx.TensorLayer(sp_ri, n_units=k)
+logits = tx.Linear(layer=ri_layer,
+                   n_units=embed_dim,
+                   shared_weights=embed.weights,
+                   bias=True)
+
+
+single_input = tx.Input(1)
+ri_input = tx.TensorLayer(tx.gather_sparse(sp_ri,single_input.tensor),k)
+
+logit = logits.reuse_with(ri_input)
 
 session = tf.InteractiveSession()
 session.run(tf.global_variables_initializer())
@@ -56,3 +71,14 @@ for batch in data_it:
 
     for ri in ris:
         print(str(ri))
+
+    print("logit shape\n", tf.shape(logits.tensor).eval())
+
+    result = logits.tensor.eval()
+    for i in range(len(result)):
+        print('{} -> {}'.format(i, result[i]))
+
+        g = logit.tensor.eval({single_input.placeholder: [[i]]})
+        g = np.reshape(g,[-1])
+        print('{} -> {}'.format(i, g))
+
